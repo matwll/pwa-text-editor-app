5941e4d05dfeb801633fbac0d3b41a4dad89e762
Matthew Williams <maw01091989@gmail.com>
2023-02-21 18:12:56 -0800

    fixed bug with header loading

--- Main/client/src/js/database.js
+++ Main/client/src/js/database.js
@@ -33,19 +33,14 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('GET from the database');
 
-  // Create a connection to the database database and version we want to use.
   const contactDb = await openDB('jate', 1);
 
-  // Create a new transaction and specify the database and data privileges.
   const tx = contactDb.transaction('jate', 'readonly');
 
-  // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
-  // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
   return result;
